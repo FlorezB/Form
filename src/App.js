1303b541bf969e4a419ae524a2f893c10104681e
Caroline <alyseflorez@hotmail.fr>
2022-07-19 11:43:32 +0200

    correction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      email: "",
+      password: "",
+      rememberMe: false,
+      emailIsValid: false,
+      passwordIsValid: false,
+      isSubmitted: false
+    }
+  }
+
+  handleEmailChange = e => {
+    const value = e.target.value
+    const regEx = new RegExp(/^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+    const isValid = regEx.test(value)
+
+    this.setState({
+      email: value,
+      emailIsValid: isValid
+    })
+  }
+
+  handlePasswordChange = e => {
+    const value = e.target.value
+    const isValid = value.length > 5
+    
+    this.setState({
+      password: value,
+      passwordIsValid: isValid
+    })
+  }
+
+  handleRememberMeChange = e => {
+    this.setState({ rememberMe: e.target.checked })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const allIsValid = this.state.passwordIsValid && this.state.emailIsValid
+
+    this.setState({ isSubmitted: allIsValid })
+  }
+
+  render() {
+    const emailInputClass = this.state.emailIsValid ? 'is-valid' : 'is-invalid'
+    const passwordInputClass = this.state.passwordIsValid ? 'is-valid' : 'is-invalid'
+
+    return (
+      <>
+        <h1 className="text-center mt-5">Login</h1>
+        <div className="container d-flex justify-content-center mt-5">
+          {
+            this.state.isSubmitted ? (
+              <div>
+                <p>{this.state.email}</p>
+              </div>
+            ) : (
+                <form
+                  className="col-10"
+                  onSubmit={this.handleSubmit}
+                >
+                  <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input
+                      type="email"
+                      className={`form-control ${emailInputClass}`}
+                      id="email"
+                      onChange={this.handleEmailChange}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input
+                      type="password"
+                      className={`form-control ${passwordInputClass}`}
+                      id="password"
+                      onChange={this.handlePasswordChange}
+                    />
+                  </div>
+                  <div className="mb-3 form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      id="remember-me"
+                      onChange={this.handleRememberMeChange}
+                    />
+                    <label className="form-check-label" htmlFor="remember-me">Remember me</label>
+                  </div>
+                  <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            )
+          }
+        </div>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
